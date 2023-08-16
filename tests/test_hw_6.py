2a8d9baef9f4f58aedcb187ea1c1c78feb1dcf11
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-16 23:34:53 +0400

    Initial commit

---
+++ tests/test_hw_6.py
@@ -0,0 +1,18 @@
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=23)
+    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+
+    if 6 <= current_time.hour < 22:
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
+
+    assert is_dark_theme is True
+
+
