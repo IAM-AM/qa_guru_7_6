3633be113a2ec1b56ca4b4d0b514ac5ad187fa44
IAM-AM <aliaksandr.manko1@gmail.com>
2023-08-21 23:57:27 +0400

    Added tests

--- tests/test_hw_6.py
+++ tests/test_hw_6.py
@@ -79,13 +79,51 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
 
+
 ''' Сделайте функцию, которая будет печатать
     читаемое имя переданной ей функции и значений аргументов.
     Вызовите ее внутри функций, описанных ниже
     
     Подсказка: Имя функции можно получить с помощью func.__name__
     Например, вызов следующей функции должен преобразовать имя функции
+    
     в более читаемый вариант (заменить символ подчеркивания на пробел,
+    
     сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
 '''
 
+
+
+
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(
+        page_url="https://companyname.com/login",
+        button_text="Register"
+    )
+
+
+def open_browser(browser_name):
+    actual_result = my_function_args(open_browser, browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = my_function_args(go_to_companyname_homepage, page_url)
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = my_function_args(find_registration_button_on_login_page, page_url, button_text)
+    assert actual_result ==\
+           "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+def my_function_args(name, *args):
+    my_func = name.__name__
+    my_func.replace("_", " ").title()
+    argm = ", ".join(args)
+
+    result = f'{name}, {argm}'
+    print(result)
